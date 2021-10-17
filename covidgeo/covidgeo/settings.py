5b9178eba8750c0bbc853f7ceafd49143cecc0d2
twotired <spinmaster@gmail.com>
2021-10-17 15:33:19 -0700

    ELK logging

--- covidgeo/covidgeo/settings.py
+++ covidgeo/covidgeo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import json_log_formatter
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,11 +42,14 @@ LOGGING = {
             'format': '{levelname} {message}',
             'style': '{',
         },
+        "json": {
+            '()': 'json_log_formatter.VerboseJSONFormatter',
+        },
     },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
+            'formatter': 'json',
         },
     },
     'root': {
