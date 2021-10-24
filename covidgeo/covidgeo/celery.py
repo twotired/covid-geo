0a9fdf35d252583feaf1481d3ad480972787f3a8
twotired <spinmaster@gmail.com>
2021-10-24 10:16:39 -0700

    configure celery logging

--- covidgeo/covidgeo/celery.py
+++ covidgeo/covidgeo/celery.py
@@ -1,6 +1,7 @@
 import os
 
-from celery import Celery, shared_task
+from celery import Celery
+from celery.signals import setup_logging
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covidgeo.settings')
 
@@ -11,6 +12,12 @@ app = Celery('covidgeo', broker='redis://redis:6379/0')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+@setup_logging.connect
+def config_loggers(*args, **kwags):
+    from logging.config import dictConfig
+    from django.conf import settings
+    dictConfig(settings.LOGGING)
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
